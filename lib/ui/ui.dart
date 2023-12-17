e860cf3c3c0c4be8221d9863ce3cf849d24ffd45
Elijah Ashitey <39532130+Elijah-Ashitey@users.noreply.github.com>
2023-12-17 10:06:23 +0000

    Add UI cards for Black Jack and Coffee UI screens

--- lib/ui/ui.dart
+++ lib/ui/ui.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_fun/ui/cards/cards.dart';
+import 'package:flutter_fun/constants.dart';
+import 'package:flutter_fun/ui/black.jack.cards/black.cards.dart';
+
+import '../models/ui.model.dart';
+import 'coffee/coffee.dart';
 
 class UICards extends StatefulWidget {
   const UICards({super.key});
@@ -27,31 +31,36 @@ class _UICardsState extends State<UICards> {
                       topLeft: Radius.circular(10),
                       topRight: Radius.circular(10))),
               child: ListView(padding: EdgeInsets.zero, children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                    child: ListTile(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => const BlackJack(),
-                            ),
-                          );
-                        },
-                        contentPadding:
-                            const EdgeInsets.symmetric(horizontal: 4),
-                        leading: ClipRRect(
-                          borderRadius: BorderRadius.circular(8.0),
-                          child: Image.network(
-                              "https://source.unsplash.com/user/c_v_r/100x100"),
-                        ),
-                        title: const Text("Black Jack"),
-                        subtitle:
-                            const Text("Card user interface for black jack"),
-                        trailing: const Icon(Icons.keyboard_arrow_right)),
-                  ),
-                )
+                ...ui
+                    .map((e) => Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Card(
+                            child: ListTile(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (_) => e.screen),
+                                  );
+                                },
+                                contentPadding:
+                                    const EdgeInsets.symmetric(horizontal: 4),
+                                leading: ClipRRect(
+                                  borderRadius: BorderRadius.circular(8.0),
+                                  child: e.image.contains('asset')
+                                      ? Image.asset(
+                                          e.image,
+                                          width: 35,
+                                          fit: BoxFit.fill,
+                                        )
+                                      : Image.network(e.image),
+                                ),
+                                title: Text(e.title),
+                                subtitle: Text(e.description),
+                                trailing:
+                                    const Icon(Icons.keyboard_arrow_right)),
+                          ),
+                        ))
+                    .toList(),
               ]),
             ),
           ),
@@ -60,3 +69,18 @@ class _UICardsState extends State<UICards> {
     );
   }
 }
+
+List<UIModel> ui = [
+  UIModel(
+    title: 'Black Jack',
+    description: 'Card user interface for black jack',
+    image: "${uiImage}blackJack.png",
+    screen: const BlackJack(),
+  ),
+  UIModel(
+    title: 'Coffee UI',
+    description: 'Beautiful coffee UI screen',
+    image: "${uiImage}coffee.ui.png",
+    screen: const CoffeeUI(),
+  ),
+];
